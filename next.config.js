c1223c0cc287d665b99d8ae9c4b3593a8ba47750
Pevita <154698257+0xPevita@users.noreply.github.com>
2026-03-14 04:23:59 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,25 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      fs: false,
-      net: false,
-      tls: false,
-    };
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+        got: false,
+        http: false,
+        https: false,
+        stream: false,
+        crypto: false,
+        path: false,
+        os: false,
+        zlib: false,
+        '@telegram-apps/bridge': false,
+        aptos: false,
+        '@mizuwallet-sdk/core': false,
+      };
+    }
     return config;
   },
 };
